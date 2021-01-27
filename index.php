e86829aea8039f66cb6a74bf2e87b4c456e8f431
Puttipob Noracharoen <63462104+puttipob@users.noreply.github.com>
2021-01-27 15:35:05 +0700

    API for checking COVID information in Thailand
    API for checking COVID information in Thailand

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <link rel="stylesheet" href="style.css">
-    <!-- CSS -->
+    <!-- Link CSS -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Covid</title>
 </head>
